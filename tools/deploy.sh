865d25e497b0aa2d94141c8d100bcdaf49132665
Robert T. McGibbon <rmcgibbo@gmail.com>
2016-02-13 12:29:15 -0800

    Push

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+OUTPUTDIR=output
+S3_BUCKET=rmcgibbo.org
+
 source activate py27
 pip install s3cmd
-s3cmd --guess-mime-type sync $(OUTPUTDIR)/ s3://$(S3_BUCKET) --access_key=$AWS_ACCESS_KEY_ID --secret_key=$AWS_SECRET_ACCESS_KEY
+
+s3cmd --guess-mime-type sync ${OUTPUTDIR}/ s3://${S3_BUCKET} --access_key=$AWS_ACCESS_KEY_ID --secret_key=$AWS_SECRET_ACCESS_KEY
