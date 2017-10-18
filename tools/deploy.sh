bc8951e8abef035b731694b0b279138dc7a600d1
Robert T. McGibbon <rmcgibbo@gmail.com>
2017-10-18 11:47:44 -0400

    Update deploy.sh

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -3,7 +3,11 @@ OUTPUTDIR=output
 S3_BUCKET=rmcgibbo.org
 
 source activate py27
-pip install s3cmd
+# pip install s3cmd
+# Until https://github.com/s3tools/s3cmd/issues/887 is released,
+# post v2.0.0
+pip install git+git://github.com/s3tools/s3cmd.git
+
 
 s3cmd sync ${OUTPUTDIR}/ s3://${S3_BUCKET} \
   --access_key=$AWS_ACCESS_KEY_ID --secret_key=$AWS_SECRET_ACCESS_KEY \
